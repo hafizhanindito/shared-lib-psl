88f7fcf1118083aaa50d47940f47689d07dc2ea2
hafizhanindito <hafizpratama@gmail.com>
2020-07-24 15:21:01 +0700

    pray no 2

--- src/com/workshop/main.groovy
+++ src/com/workshop/main.groovy
@@ -2,10 +2,15 @@
 package com.workshop
  
 import com.workshop.Config
+import com.workshop.utils
+import com.workshop.stages.*
+
  
 def main(script) {
    // Object initialization
    c = new Config()
+   u = new utils()
+   sprebuild = new prebuild()
  
    // Pipeline specific variable get from injected env
    // Mandatory variable will be check at details & validation steps
@@ -15,7 +20,10 @@ def main(script) {
    def docker_user = ("${script.env.docker_user}" != "null") ? "${script.env.docker_user}" : ""
    def app_port = ("${script.env.app_port}" != "null") ? "${script.env.app_port}" : ""
    def pr_num = ("${script.env.pr_num}" != "null") ? "${script.env.pr_num}" : ""
- 
+
+   // Initialize docker tools
+   def dockerTool = tool name: 'docker', type: 'dockerTool'
+
    // Pipeline object
    p = new Pipeline(
        repository_name,
@@ -23,16 +31,20 @@ def main(script) {
        git_user,
        docker_user,
        app_port,
-       pr_num
+       pr_num,
+       dockerTool
    )
  
    ansiColor('xterm') {
        stage('Pre Build - Details') {
-           // TODO: Call pre build details function
+                 u.checkPrStatus(p)
+           sprebuild.validation(p)
+           sprebuild.details(p)
+
        }
  
        stage('Pre Build - Checkout & Test') {
-           // TODO: Call pre build checkout & test function
+       sprebuild.checkoutBuildTest(p)
        }
  
        stage('Build & Push Image') {
